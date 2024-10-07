4d2ccde16b638f056c05124267123cab434284fa
leandraff7 <87551352+leandraff7@users.noreply.github.com>
2024-10-06 22:44:52 -0300

    up

--- assets/js/script.js
+++ assets/js/script.js
@@ -120,7 +120,7 @@ $(document).ready(function () {
 
     // Exemplo de valor arrecadado e meta
     const totalCampaignGoal = 68378.20;  // Meta de arrecadação total
-    const amountRaised = 10673.00;              // Quantia arrecadada até agora
+    const amountRaised = 10773.00;              // Quantia arrecadada até agora
     const remainingAmount = totalCampaignGoal - amountRaised;
 
     donationChart = new Chart(ctx, {
